245d3afed12e99ef9b02ac6020dcc707df84ddb8
hamooza03 <abualkhair.hamza03@gmail.com>
2025-12-22 16:15:33 -0500

    Adders!

--- 1.Half Adder/half_adder.v
+++ 1.Half Adder/half_adder.v
@@ -1,10 +1,12 @@
+`timescale 1ns / 1ps
+
 module half_adder(
     input a,
     input b,
     output sum,
-    output carry,
+    output carry
 );
     assign sum = a ^ b;
-    assign carry a & b;
+    assign carry = a & b;
 
 endmodule
--- 1.Half Adder/half_adder_tb.v
+++ 1.Half Adder/half_adder_tb.v
@@ -1,5 +1,7 @@
 module half_adder_tb();
-    wire a, b, sum, carry;
+    reg a, b;
+    wire sum, carry;
+
     half_adder dut(
         .a(a),
         .b(b),
@@ -7,11 +9,17 @@ module half_adder_tb();
         .carry(carry)
     );
     initial begin
-        $monitor("a=%b, b=%b => sum=%b, carry=%b", a, b, sum, carry);
+        $dumpfile("half_adder_tb.vcd");
+        $dumpvars(0, half_adder_tb);
+
+        $display("A B | SUM CARRY");
+        $display("--------------");
+        $monitor("%b %b |  %b   %b", a, b, sum, carry);
+
         a = 0; b = 0; #10;
         a = 0; b = 1; #10;
         a = 1; b = 0; #10;
         a = 1; b = 1; #10;
-        $finish;
+        #1 $finish;
     end
 endmodule
