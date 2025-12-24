b838f3402e52630fa6d1acc6f7fa4b2e77acbc35
hamooza03 <abualkhair.hamza03@gmail.com>
2025-12-23 19:19:07 -0500

    CLA & Subtactor!

---
+++ 5.Carry Look Ahead Adder/cla.v
@@ -0,0 +1,24 @@
+module cla(
+    input [3:0] a,
+    input [3:0] b,
+    input cin,
+    output [3:0] sum,
+    output cout
+);
+    wire [3:0] g, p;
+    wire [3:0] c;
+
+    assign c[0] = cin;
+
+    assign g = a & b;
+    assign p = a ^ b;
+
+    assign c[1] = g[0] | (p[0] & c[0]);
+    assign c[2] = g[1] | (p[1] & g[1]) | (p[1] & p[0] & c[0]);
+    assign c[3] = g[2] | (p[2] & g[2]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & c[0]);
+    assign c[4] = g[3] | (p[3] & g[3]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & c[0]);
+
+    assign sum = p ^ c[3:0];
+    assign cout = c[4];
+
+endmodule
---
+++ 6.Subtractor/subtractor.v
@@ -0,0 +1,48 @@
+module subtractor(
+    input signed [3:0] a,
+    input signed [3:0] b,
+    output signed [3:0] diff,
+    output out
+);
+    // 2s Complement Subtraction needs to add 1 to inverted b
+    wire in = 1'b1;
+    // invert b
+    wire [3:0] b_comp;
+    assign b_comp = ~b;
+
+    wire c1, c2, c3;
+
+    full_adder fa0(
+        .a(a[0]),
+        .b(b_comp[0]),
+        .cin(in),
+        .sum(diff[0]),
+        .cout(c1)
+    );
+
+    full_adder fa1(
+        .a(a[1]),
+        .b(b_comp[1]),
+        .cin(),
+        .sum(diff[1]),
+        .cout(c2)
+    );
+
+    full_adder fa2(
+        .a(a[2]),
+        .b(b_comp[2]),
+        .cin(),
+        .sum(diff[2]),
+        .cout(c3)
+    );
+
+    full_adder fa3(
+        .a(a[3]),
+        .b(b_comp[3]),
+        .cin(),
+        .sum(diff[3]),
+        .cout(out)
+    );
+
+
+endmodule
